ef3e219d010e0de11d5b6c0a4e6b1ab376f042a5
jahangir091 <jahangir.cse09@gmail.com>
2018-08-12 16:22:21 +0600

    fixed mail templates

--- geonode/catalog/urls.py
+++ geonode/catalog/urls.py
@@ -7,12 +7,12 @@ urlpatterns = patterns(
     'geonode.feedback.views',
 
     # home page section management with image and texts
-    url(r'^list/(?P<org>\w+)/$', DataCatalogList.as_view(), name='data_catalog_list'),
+    url(r'^list/(?P<org>[-\w]+)/$', DataCatalogList.as_view(), name='data_catalog_list'),
     url(r'^create$', DataCatalogCreate.as_view(), name='data_catalog_create'),
 
     url(r'^edit/(?P<cat_pk>\d+)$', DataCatalogUpdate.as_view(), name='data_catalog_edit'),
     url(r'^delete/(?P<cat_pk>\d+)$', DataCatalogDelete.as_view(), name='data_catalog_delete'),
 
-    url(r'^download/request/(?P<org>\w+)/$', CatDownloadRequest.as_view(), name='download_cat_request'),
+    url(r'^download/request/(?P<org>[-\w]+)/$', CatDownloadRequest.as_view(), name='download_cat_request'),
 
 )
