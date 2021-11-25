27f5b82c1b8419512db5a8bf07eb870fb7b2905a
root <root@nsdi.gov.bd>
2021-11-25 16:42:03 +0600

    merged migrations

---
+++ geonode/feedback/migrations/0007_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('feedback', '0006_auto_20180813_1128'),
+        ('feedback', '0006_auto_20211013_2003'),
+    ]
+
+    operations = [
+    ]
